1ae527fc6950f4933156c6e8b32d6d353cf325ce
Your Nameclear <you@example.com>
2021-06-18 02:12:25 -0700

    assigning random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*more headers goes there*/
+/* more headers goes there */
 
-/*
- * main - Entry point
- *
- * Return: always 0
- */
+/* betty style doc for function main goes there */
 
 int main(void)
 {
