fca4c5b76a9e4279040ba6744da5efc8d388076a
Your Nameclear <you@example.com>
2021-06-18 03:01:29 -0700

    printing alphabet in lower/upper

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (lastNum == 0)
 		printf("Last digit of %i is %i and is 0\n", n, lastNum);
 	else if (lastNum > 5)
