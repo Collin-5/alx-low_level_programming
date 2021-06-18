2902f5b9a042ce709ad1ddafd3862502e00296b8
Your Nameclear <you@example.com>
2021-06-18 09:59:25 -0700

    listing alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet reverse
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(i);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
