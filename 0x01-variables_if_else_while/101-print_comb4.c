8eebf31bcf5417852c0a280cb0149bb041cf8d67
Your Nameclear <you@example.com>
2021-06-18 12:30:31 -0700

    listing numbers combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - printing number comp]binations
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+	int third;
+
+	for (first = '0'; first <= '9'; first++)
+	{
+		for (second = (first + 1); second <= '9'; second++)
+		{
+			for (third = (second + 1); third <= '9'; third++)
+			{
+				putchar(first);
+				putchar(second);
+				putchar(third);
+
+				if (first != '7' || second != '8' || third || third != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n')
+
+	return (0);
+}
