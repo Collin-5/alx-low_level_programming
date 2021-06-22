25f9c6eed2413a5f086572756f68a51768a80a18
Your Nameclear <you@example.com>
2021-06-22 00:31:12 -0700

    printing alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+
+/**
+ * print_alphabet_x10 - print alphabet lowercase ten times
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void);
+{
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+	}
+}
