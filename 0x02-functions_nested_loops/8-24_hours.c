bdc9b7a332fb081e62260090fa6769c6cd27111f
Your Nameclear <you@example.com>
2021-06-22 04:10:55 -0700

    couting minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: last digit of a number
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			if (h < 10)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else if (h >= 10)
+			{
+				_putchar(h / 10 + '0');
+				_putchar((h % 10) + '0');
+			}
+
+			_putchar(':');
+
+			if (m < 10)
+			{
+				_putchar('0');
+				_putchar(m + '0');
+			}
+			else if (m >= 10)
+			{
+				_putchar(m / 10 + '0');
+				_putchar((m % 10) + '0');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
