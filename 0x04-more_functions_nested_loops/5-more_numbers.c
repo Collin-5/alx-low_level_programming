11d5f055857bb6ffe11662a30343dc96a27b6bce
Collin-5 <collinsotieno380@gmail.com>
2021-06-24 02:57:59 -0700

    printing numbers x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - printing numbers 10x
+ *
+ * Return: return multiple numbers
+ */
+
+void more_numbers(void)
+{
+	char i;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
