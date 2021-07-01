9541fb3b82e4c0c1dae21c2a630bf5b4e40f7107
Collin-5 <collinsotieno380@gmail.com>
2021-07-01 04:55:55 -0700

    correcting

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,28 @@
 #include "holberton.h"
 
 /**
- * leet - encode string into 1337 leet
- * @s: string to manipulate
- * Return: string
+ *leet - encodes a string into 1337
+ *
+ *@s: string being encoded
+ *Return: returns a string encoded in 1337
  */
 
 char *leet(char *s)
 {
+	int x, y, upper;
+	char letter[5] = {'a', 'e', 'o', 't', 'l'};
+	char number[5] = {'4', '3', '0', '7', '1'};
 
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	upper = -32;
 
-	int i, j;
-
-	for (j = 0; s[j] != '\0'; j++)
+	for (x = 0; *(s + x) != '\0'; x++)
 	{
-		for (i = 0; a[i] != '\0'; i++)
+		for (y = 0; *(letter + y) != '\0'; y++)
 		{
-			if (s[j] == a[i])
-				s[j] = b[i];
+			if (*(s + x) == *(letter + y) ||
+				*(s + x) == *(letter + y) + upper)
+				*(s + x) = *(number + y);
 		}
 	}
-
 	return (s);
-
+}
