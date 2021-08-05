a47060d53ab69f6433645b501ce0e6e517fb9cd8
Collin-5 <collinsotieno380@gmail.com>
2021-08-05 03:04:04 -0700

    one

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,58 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * _strlen - compute string const len
+ * @s: takes a string
+ * Return: length
  */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * binary_to_uint - conver binary to unsigned int
+ * @b: string to be converted
+ * Return: decimal number
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 
-	int len = 0, i;
 	unsigned int sum = 0;
+	unsigned int base_2 = 1;
+	int len;
+	unsigned int conv;
 
-	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	if (b != NULL)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		len = _strlen(b) - 1;
 
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		while (len >= 0)
+		{
+			conv = b[len] - '0';
+
+			if (conv != 1 && conv != 0)
+				return (0);
+
+			if (conv == 0)
+				sum += 0;
+
+			if (conv == 1)
+				sum += base_2;
+
+			len--;
+			base_2 *= 2;
+		}
+		return (sum);
 	}
-
-	return (sum);
+	return (0);
 }
+
